737fab274fff9d157a8dae1b8852c0cbf94d1c79
GabDevCode <kr4v3n27@gmail.com>
2026-03-04 19:21:52 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/reto_adivina_el_numero/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto_adivina_el_numero
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _AdivinaElnumeroState();
+}
+
+class _AdivinaElnumeroState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
